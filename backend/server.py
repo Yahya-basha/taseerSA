2705fc85e7f84f6a35e3a754b47d1a7f2893007c
Yahya-basha <yahya9031@gmail.com>
2026-01-15 14:34:29 -0500

    Fix: Improve project structure and Vercel configuration

--- backend/server.py
+++ backend/server.py
@@ -25,9 +25,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[os.environ.get('DB_NAME', 'taseer_db')]
 
 # Security
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
